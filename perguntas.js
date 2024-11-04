c556fe54cd3ccd7baed4911b7e38329427dab892
rafaela5741 <00001108081903sp@al.educacao.sp.gov.br>
2024-11-04 10:52:52 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Matemática',
+    'Quanto é 2 + 2?',
+    'A resposta é 4'
+)
+
+criaCartao(
+    'Técnologia e Robótica',
+    'Qual o comando que salva uma ação no VsCode?',
+    'Ctrl S'
+)
+
+criaCartao(
+    'Empreendedorismo',
+    'O que é um plano de negócio?',
+    'É um documento que descreve os objetivos de uma empresa,as estratégias para alcança-los e os recursos necessários para isso.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Qual é a tradução de cachorro em Inglês?',
+    'Resposta= Dog'
+)
